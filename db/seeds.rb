6edb72f2a25c03119587bc4010adfd8b17530971
Priyanka Kumbhar <priyankakumbhar789@gmail.com>
2016-09-20 11:22:43 +1000

    changed image extension in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,17 +10,17 @@ p "User Count: #{User.all.count} "
 
 Shop.destroy_all
 
-s1 = Shop.create :name => "Sardough Coffee", :description => "It's very good" ,:image => "6d68bbd502a2dbbcde2d44b4af84b1ed_ksxswl"
-s2 = Shop.create :name => "7 eleven ", :description => "It's very cheap " ,:image => "cabukee-counter_kqvvgl"
-s3 = Shop.create :name => "Type B ", :description => "Joes Gilfriend Cafe !! " ,:image => "ultimo-coffee_q5c15s"
+s1 = Shop.create :name => "Sardough Coffee", :description => "It's very good" ,:image => "6d68bbd502a2dbbcde2d44b4af84b1ed_ksxswl.jpg"
+s2 = Shop.create :name => "7 eleven ", :description => "It's very cheap " ,:image => "cabukee-counter_kqvvgl.jpg"
+s3 = Shop.create :name => "Type B ", :description => "Joes Gilfriend Cafe !! " ,:image => "ultimo-coffee_q5c15s.jpg"
 
 p "Shops Count: #{Shop.all.count}"
 
 Product.destroy_all
 
-p1 = Product.create :name => "Late", :price => 5, :description => "It's coffe and milk ",:image => "0b8c3cdf67e5044cb640c8fa4464edf4_vx2ajg"
-p2 = Product.create :name => "Capuccino", :price => 4, :description => "It's coffe and milk and chocollate  ",:image => "0723b338d9fa70d5a92ee496034c9b61_opu65z"
-p3 = Product.create :name => "Sandwich", :price => 9, :description => "Very very expensive !! ",:image => "029fb535dffd87cbc90b568a84fb76ca_gz0gmd"
+p1 = Product.create :name => "Late", :price => 5, :description => "It's coffe and milk ",:image => "0b8c3cdf67e5044cb640c8fa4464edf4_vx2ajg.jpg"
+p2 = Product.create :name => "Capuccino", :price => 4, :description => "It's coffe and milk and chocollate  ",:image => "0723b338d9fa70d5a92ee496034c9b61_opu65z.jpg"
+p3 = Product.create :name => "Sandwich", :price => 9, :description => "Very very expensive !! ",:image => "029fb535dffd87cbc90b568a84fb76ca_gz0gmd.jpg"
 
 s1.products << p1
 s1.products << p2
