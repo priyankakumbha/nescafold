8be47dec7a195bdec64d4b72a68af86f5ec7432b
lucasviela <fleminglucas@gmail.com>
2016-09-22 09:26:19 +1000

    twilio

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,11 +12,6 @@ class MessagesController < ApplicationController
   )
   end
 
-  def reply
-     client = Twilio::REST::Client.new Rails.application.secrets.PN0d8b9f8b8a9ba586ed6878aeb21f9d88, Rails.application.secrets.9ee47e555c59ff5c3b8adbd8f631374f
-     message = client.messages.create from: '+61428257903', to: '+0416404185', body: 'First ever MyMedy notifcation test.'
-     render plain: message.status
-   end
 
   private
 
