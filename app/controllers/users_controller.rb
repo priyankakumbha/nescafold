14360f847be01f3df593f73a8e617933c39bc63a
lucasviela <fleminglucas@gmail.com>
2016-09-22 16:28:46 +1000

    css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def test
-    
+
   end
   # GET /users/1
   # GET /users/1.json
@@ -33,8 +33,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
 
-      someController = NotificationsController.new
-      someController.send_message(@user.phone_number , '', @image_url);
+
 
       if @user.save
         UserMailer.welcome(@user).deliver_now
